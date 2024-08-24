98ce3ec3e0e516ffc0fbd109db9529d53101000e
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-08-24 06:16:44 +0500

    fix: "changes"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,13 +14,13 @@ export default function LayoutMain() {
                 </div>
                 <div className='col-span-1 w-full h-auto lg:h-[50vh] grid grid-cols-1 lg:grid-cols-2 gap-3 p-5 overflow-visible'>
                   <motion.div
-                        className="col-span-1 rounded-lg shadow-2xl p-5 flex flex-col justify-center gap-y-3 bg-red-100 font-[Montserrat]"
+                        className="col-span-1 rounded-lg shadow-2xl p-5 flex flex-col justify-center gap-y-3 bg-slate-200 font-[Montserrat]"
                         initial={{ y: -250, opacity: 0 }}
                         animate={{ y: 0, opacity: 1 }}
                         transition={{ type: 'spring', stiffness: 50, damping: 10 }}
                     >
                      <p className='text-lg 2xl:text-2xl font-semibold text-[#BE261F]'>Request Free Designs:</p>
-                     <p className='bg-slate-200 text-black w-1/2 py-1 text-center rounded-full font-bold text-sm'>Get Design in 24 Hours</p>
+                     <p className='bg-gray-200 text-black w-1/2 py-1 text-center rounded-full font-bold text-sm'>Get Design in 24 Hours</p>
                      <p>
                      Let one of our in-house designers take it from here!  All designs and revisions are always free.
                      </p>
@@ -30,13 +30,13 @@ export default function LayoutMain() {
                      <FreeDesignSlider />
                     </motion.div>
                     <motion.div
-                       className="col-span-1 rounded-lg shadow-2xl p-5 flex flex-col justify-center gap-y-3 bg-red-100 font-[Montserrat]"
+                       className="col-span-1 rounded-lg shadow-2xl p-5 flex flex-col justify-center gap-y-3 bg-slate-200 font-[Montserrat]"
                         initial={{ x: 250, opacity: 0 }}
                         animate={{ x: 0, opacity: 1 }}
                         transition={{ type: 'spring', stiffness: 50, damping: 10 }}
                     >
                      <p className='text-2xl font-semibold text-[#BE261F]'>Request Free Designs:</p>
-                     <p className='bg-slate-200 text-black w-1/2 py-1 text-center rounded-full font-bold text-sm'>Design your Own</p>
+                     <p className='bg-gray-200 text-black w-1/2 py-1 text-center rounded-full font-bold text-sm'>Design your Own</p>
                      <p>
                      Upload your logo and choose from hundreds of templates and colors.
                      </p>
