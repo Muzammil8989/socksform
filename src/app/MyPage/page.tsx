9ad70df0c9ef970ce527b26dd176f0eac9d80457
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2024-08-29 04:48:49 +0500

    fix: "changes"

--- src/app/MyPage/page.tsx
+++ src/app/MyPage/page.tsx
@@ -73,6 +73,7 @@ const PageComponent = () => {
 
                 </div>
             </section>
+
             <section className='h-[90vh] w-full px-20 py-24 bg-no-repeat bg-center bg-cover grid grid-cols-3 font-[Montserrat]'
                 style={{ backgroundImage: "url('/2ndSectionBGImage.png')" }}
             >
@@ -173,8 +174,8 @@ const PageComponent = () => {
                 <div className='w-full grid grid-cols-2 items-center justify-between py-14 px-56'>
                     <p className='font-semibold text-5xl text-[#333D62]'>Whats Included </p>
                     <p className='text-lg font-normal'>
-                        What's included in the Standard Package for your web
-                        design services?" I can provide a list of features and
+                        Whats included in the Standard Package for your web
+                        design services? I can provide a list of features and
                         deliverables that are typically part.
                     </p>
                 </div>
@@ -208,10 +209,9 @@ const PageComponent = () => {
                     <div className='col-span-1 w-full rounded-lg space-y-5 relative bg-white'
                     >
                         <p className='font-semibold text-2xl text-[#333D62] pt-5 px-5'>GI glasses, with your choice of lens</p>
-                        <p className='px-5'>Glasses are more than just a corrective aid; they're a
+                        <p className='px-5'>Glasses are more than just a corrective aid; theyre a
                             fashion statement. With the right choice of lens</p>
                         <div className='overflow-hidden'
-                        // style={{ backgroundImage: "url('/glassmall.png')" }}
                         >
                             <img src="/glassmall.png" alt="" className='absolute bottom-0 left-0' />
                         </div>
@@ -245,6 +245,65 @@ const PageComponent = () => {
                 </div>
             </section>
 
+            <section className='w-full h-[75vh] font-[Montserrat] bg-cover bg-center flex justify-normal items-end'
+                style={{
+                    backgroundImage: "url('/aftergridbgImage.png')",
+                }}
+            >
+                <div className="h-1/2 w-1/2 flex justify-center items-center font-semibold text-6xl text-white bg-gradient-to-t from-black via-30% to-transparent">
+                    Our Story
+                </div>
+
+                <div className="relative h-1/2 w-1/2 p-12 rounded-t-xl  bg-opacity-65 backdrop-blur-lg  bg-gray-500/60">
+                    <p className="relative text-2xl tracking-wide w-10/12 z-10 text-white">
+                        Undisturbed connections. We explore human experiences
+                        where digital and physical realities merge. We are driven by
+                        an ambition to make people thrive and advance beyond
+                        their screens - while still being connected to everything
+                        digital. All we really want is to make people see what
+                        matters when it matters.
+                    </p>
+                    <div className='flex gap-x-6 py-5'>
+                        <button className="bg-white rounded-full px-10 py-4 text-black font-medium">
+                            Know More
+                        </button>
+                        <span className="w-14 h-14 bg-white rounded-full flex justify-center items-center -ml-6">
+                            <img src="/arrowIconBlack.png" width={22} height={22} alt="Arrow Icon" />
+                        </span>
+                    </div>
+                </div>
+            </section>
+
+            <section className='w-full h-[70vh] bg-[#181818] font-[Montserrat]'>
+                <div className='grid grid-cols-2 h-full'>
+                    <div className='col-span-1 flex justify-center items-center'>
+                        <div className='flex flex-col gap-y-4 items-start justify-normal w-2/3'>
+                            <span className='rounded-lg  bg-opacity-65 backdrop-blur-lg  bg-white/10 px-3 py-2 text-white '>
+                                $599 / No subscription
+                            </span>
+                            <h1 className='text-6xl text-white'>Personalize your G1</h1>
+                            <p className='text-lg text-white'>Iconic and timeless Panto design. Flatters most face shapes
+                                and lives with any look from traditional to on-trend. A
+                                versatile choice that will never look dated.</p>
+                            <div className='flex gap-x-6 py-5'>
+                                <button className="bg-white rounded-full px-10 py-4 text-black font-medium">
+                                    Pre Order
+                                </button>
+                                <span className="w-14 h-14 bg-white rounded-full flex justify-center items-center -ml-6">
+                                    <img src="/arrowIconBlack.png" width={22} height={22} alt="Arrow Icon" />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-span-1 bg-no-repeat bg-contain bg-left relative'
+                        style={{
+                            backgroundImage: "url('/bgImageLastsection.png')"
+                        }}
+                    >
+                        <img src="lastSectionMobileImage.png" alt="" className='absolute bottom-0 ' />
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
